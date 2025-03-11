b16c9150e2908baa09ad9366bb70cbc21dd9d318
HectorFranco-MISO <h.franco@uniandes.edu.co>
2025-03-11 17:33:11 -0500

    Validator passwords

--- VitaAlarm_Web/src/app/pages/register-email/register-email.component.ts
+++ VitaAlarm_Web/src/app/pages/register-email/register-email.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-email',
   templateUrl: './register-email.component.html',
-  styleUrls: ['./register-email.component.scss']
+  styleUrls: ['./register-email.component.scss'],
 })
 export class RegisterEmailComponent {
-  password: string = ''
+  name: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(private router: Router) {}
 
   goBack() {
-    window.history.back()
+    window.history.back();
   }
 
+  register() {
+    if (this.password === this.confirmPassword && this.name.length > 5) {
+      setTimeout(() => {
+        this.router.navigate(['/escritorio']);
+      }, 300);
+    }
+  }
 }
