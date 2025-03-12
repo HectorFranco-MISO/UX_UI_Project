c52f3c48277dc5dd5f5e0955fef869acc3f13fa7
HectorFranco-MISO <h.franco@uniandes.edu.co>
2025-03-12 16:11:05 -0500

    Onboarding screen done

--- VitaAlarm_Movil/app/src/main/java/co/edu/uniandes/misw4203/vitaalarm_movil/OnboardingActivity.kt
+++ VitaAlarm_Movil/app/src/main/java/co/edu/uniandes/misw4203/vitaalarm_movil/OnboardingActivity.kt
@@ -8,24 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginActivity)) { v, insets ->
+        setContentView(R.layout.activity_onboarding)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.onboardingActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Referencia al botón de login
-        val loginButton: Button = findViewById(R.id.btnLogin)
-
-        // Agregar el listener para navegar a LoginActivity
-        loginButton.setOnClickListener {
-            val intent = Intent(this, OnboardingActivity::class.java)
-            startActivity(intent)
         }
     }
 }
