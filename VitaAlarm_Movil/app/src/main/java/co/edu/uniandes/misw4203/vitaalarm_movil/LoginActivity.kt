5eac84a943d54d7e47a1fa1510f2c6565a59edf2
HectorFranco-MISO <h.franco@uniandes.edu.co>
2025-03-12 00:09:24 -0500

    LoginActivity almost done

--- VitaAlarm_Movil/app/src/main/java/co/edu/uniandes/misw4203/vitaalarm_movil/LoginActivity.kt
+++ VitaAlarm_Movil/app/src/main/java/co/edu/uniandes/misw4203/vitaalarm_movil/LoginActivity.kt
@@ -1,31 +1,20 @@
 package co.edu.uniandes.misw4203.vitaalarm_movil
 
 import android.os.Bundle
-import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Referencia al botón de login
-        val loginButton: Button = findViewById(R.id.btn_login)
-
-        // Agregar el listener para navegar a LoginActivity
-        loginButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
         }
     }
 }
